cba13eac2f41663eaf3fc9027ce997de89def387
Ivan Vorobiov <vorobiov.ivan01@gmail.com>
2022-04-01 16:44:12 +0300

    solution

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="calculator">
+    <img v-bind:src="'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Fibonacci_Spiral.svg/300px-Fibonacci_Spiral.svg.png'">
+
+    <h2 class="title">
+      Fibonacci Calculator
+    </h2>
+
+    <label for="input">
+      <input
+        class="input is-normal"
+        placeholder="Input"
+        type="text"
+        id="input"
+        v-bind:value="inputValue"
+        v-on:input="inputChangeHandler"
+      >
+
+      <input
+        class="input is-normal"
+        readonly
+        placeholder="Result"
+        type="text"
+        v-bind:value="fibonacci"
+      >
+    </label>
+
+    <button
+      class="button is-success is-medium"
+      v-on:click="fibonacci = calculateFibonacci(inputValue)"
+    >
+      Calculate
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data () {
+    return {
+      inputValue: '',
+      fibonacci: ''
+    }
+  },
   components: {
-    HelloWorld
+  },
+  methods: {
+    inputChangeHandler (event) {
+      this.inputValue = event.target.value
+    },
+    calculateFibonacci (num) {
+      return (
+        num <= 1
+          ? num
+          : this.calculateFibonacci(num - 1) + this.calculateFibonacci(num - 2)
+      )
+    }
   }
 }
 </script>
 
 <style>
+.calculator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+}
+
+img {
+  margin: 50px 0;
+  object-fit: contain;
+  width: 200px;
+  border-radius: 2px;
+}
+
+.input {
+  display: block;
+  margin: 20px 0;
+  width: 80vw;
+}
+
+.button {
+  position: absolute;
+  bottom: 25vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/vue-test-task-gh/',
   transpileDependencies: true
 })
